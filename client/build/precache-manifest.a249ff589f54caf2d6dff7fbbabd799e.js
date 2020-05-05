593d5c6ece816c363fe806761950b2891c16069e
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-05-05 07:08:29 +0530

    created a npm build and uploading new files to heroku

---
+++ client/build/precache-manifest.a249ff589f54caf2d6dff7fbbabd799e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf3965b8967d6e1bc72de230e882675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a298f08f5cea276fb34d",
+    "url": "/static/css/main.4954e909.chunk.css"
+  },
+  {
+    "revision": "c7a571805489723c77ac",
+    "url": "/static/js/2.1f784fba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f784fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a298f08f5cea276fb34d",
+    "url": "/static/js/main.a79a3498.chunk.js"
+  },
+  {
+    "revision": "c0d5acbabd57bcd0ee3e",
+    "url": "/static/js/runtime-main.4225bf66.js"
+  }
+]);
