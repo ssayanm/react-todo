86206b5735684d856ab5e696d032df3a08b8bafe
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-12-23 18:25:44 +0530

    updated about

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -8,11 +8,10 @@ const About = () => {
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, printer took a galley of type and scrambled it to make
-        a type specimen book.
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
+        a type specimen book.It was popularised in the 1960s with the release of
+        Letraset sheets containing Lorem Ipsum passages, and more recently with
+        desktop publishing software like Aldus PageMaker including versions of
+        Lorem Ipsum.
       </p>
     </div>
   );
